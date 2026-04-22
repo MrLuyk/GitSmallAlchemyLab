0395fa919f00e58cf3bcb31e3d842865008c4fea
Mr. Luyk <133881094+MrLuyk@users.noreply.github.com>
2026-04-22 11:47:43 -0400

    Added a camera script and character movement script. Also shifted the back-to-back walls so they didn't glitch and a few columns for even placement

---
+++ Assets/Scripts/FirstPersonCamera.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FirstPersonCamera : MonoBehaviour
+{
+    private float xAxis;
+    private float yAxis;
+
+    float xAxisTurnRate = 540f;
+    float yAxisTurnRate = 540f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    void Update()
+    {
+        float xAxisInput = Input.GetAxis("Mouse Y");
+        float yAxisInput = Input.GetAxis("Mouse X");
+
+        xAxis -= xAxisInput * xAxisTurnRate * Time.deltaTime;
+        xAxis = Mathf.Clamp(xAxis, -90f, 90f);
+        yAxis += yAxisInput * yAxisTurnRate * Time.deltaTime;
+
+        Quaternion newRotation = Quaternion.Euler(xAxis, yAxis, 0f);
+
+        Camera.main.transform.rotation = newRotation;
+    }
+}
